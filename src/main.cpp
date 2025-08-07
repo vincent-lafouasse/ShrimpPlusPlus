6e6c1e15fda31a5d19016121e859974026260c97
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-08-07 13:04:14 +0200

    load file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
+#include <cassert>
+#include <fstream>
 #include <iostream>
 
-int main() {
-	std::cout << "young krillionaire in da place" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./fryer script.rice" << std::endl;
+        std::exit(1);
+    }
+
+    const char* scriptPath = argv[1];
+    std::cout << "input is " << scriptPath << std::endl;
+
+    std::ifstream script(scriptPath);
+    if (!script.good()) {
+        std::cerr << "Failed to open script" << std::endl;
+        std::exit(1);
+    }
 }
