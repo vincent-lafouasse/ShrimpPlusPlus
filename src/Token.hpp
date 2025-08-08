06404ce3dc67ae6413656b483aed71a82cc4e6c5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-08-08 12:17:42 +0200

    sketch token types

--- src/Token.hpp
+++ src/Token.hpp
@@ -7,8 +7,21 @@
 class Token {
    public:
     enum class Kind {
+      // keywords
+        Function,
+        Let,
+      // operators
         LeftParen,
         RightParen,
+        LeftBrace,
+        RightBrace,
+        SemiColon,
+        Plus,
+        Minus,
+        Times,
+        Divides,
+      // misc
+        Identifier,
         Eof,
     };
 
