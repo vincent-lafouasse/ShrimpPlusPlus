87e1b2f038c4cd66243f3bec2839b32e134bd7c3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-08-08 12:23:30 +0200

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,23 @@
 
 #include "Token.hpp"
 
+// driver
+void runInterpreter();
+void runFile(const char* path);
+
+int main(int argc, char* argv[]) {
+    if (argc > 2) {
+        std::cerr << "Usage: ./fryer [script.rice]" << std::endl;
+        std::exit(1);
+    } else if (argc == 2) {
+        const char* scriptPath = argv[1];
+        runFile(scriptPath);
+    } else {
+        runInterpreter();
+    }
+}
+
+
 void runInterpreter() {}
 
 void runFile(const char* path) {
@@ -27,17 +44,5 @@ void runFile(const char* path) {
     for (std::size_t i = 0; i < statements.size(); ++i) {
         std::cout << "Statement " << i << ":\t";
         std::cout << statements[i] << '\n';
-    }
-}
-
-int main(int argc, char* argv[]) {
-    if (argc > 2) {
-        std::cerr << "Usage: ./fryer [script.rice]" << std::endl;
-        std::exit(1);
-    } else if (argc == 2) {
-        const char* scriptPath = argv[1];
-        runFile(scriptPath);
-    } else {
-        runInterpreter();
     }
 }
